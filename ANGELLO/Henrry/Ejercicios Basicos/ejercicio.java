820060ff3c105b94ad7f5e3ab6ca701e050672b2
Angello-06 <angellosalas06@gmail.com>
2026-09-10 20:32:42 -0500

    ejercicio java
    realice el ejercicio de java que teníamos que desarrollar, además puse las diferencias entre los lenguajes trabajados como comentarios en el archivo .java

---
+++ ANGELLO/Henrry/Ejercicios Basicos/ejercicio.java
@@ -0,0 +1,18 @@
+public class ejercicio {
+
+    public static void main(String[] args) {
+
+        String k = "19";
+
+        System.out.println("Mi edad es " + k + " años");
+    }
+    // Java, Dart y Visual Basic tienen una sintaxis diferente para realizar las
+    // mismas tareas.
+    // La diferencia principal es la forma de escribir el código, aunque los tres
+    // pueden realizar la misma tarea.
+    // Java es más estricto y requiere una estructura como public static void main,
+    // Dart tiene una sintaxis más sencilla y utiliza void main(), mientras que
+    // Visual Basic usa una sintaxis más cercana al
+    // lenguaje natural, por ejemplo Dim para declarar variables y Console.WriteLine
+    // para mostrar información.
+}
