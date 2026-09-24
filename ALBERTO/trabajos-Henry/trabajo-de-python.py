ce258c8f5ceed2281f48637e6cd10297524d39ad
Alberto Mario Perez Castro <albertomarioperezcastro@gmail.com>
2026-09-24 18:36:04 -0500

    comence con el trabajo python
    deje una parte del trabajo de python, falta una segunda parte

---
+++ ALBERTO/trabajos-Henry/trabajo-de-python.py
@@ -0,0 +1,3 @@
+print("le vamos a dar el promedio de sus notas, a continuacion vera donde ingresar las notas ")
+A = float(input("digite su primera nota: "))
+B = float(input("digite su segunda nota: "))
