839e25cc520fa8c3ad2b220caf738fa7fc0b9fa6
luluci <dannypayares13@gmail.com>
2026-09-24 22:03:40 -0500

    Añadir la primera parte de promedios en Dart
    Añade el punto de entrada principal para un programa en Dart que solicita la primera calificación e introduce el flujo de cálculo del promedio de cinco notas.

--- DANITZHA/primer-trimestre/Henry/promedio.dart
+++ DANITZHA/primer-trimestre/Henry/promedio.dart
@@ -1,2 +1,9 @@
 import 'dart:io';
 
+void main() {
+  print("Vamos a calcular el promedio de cinco notas con dart");
+
+  print("Ingrese la primera nota: ");
+  double nota1 = double.parse(stdin.readLineSync()!);
+
+}
