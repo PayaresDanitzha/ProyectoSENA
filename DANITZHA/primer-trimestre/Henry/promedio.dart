a4cd64934749e4b2cbff985f4961dfe3b1bd38cf
luluci <dannypayares13@gmail.com>
2026-09-24 22:10:33 -0500

    Digité el resto de calificaciones
    Agregué los ultimos print(). En el lee, va a convertir los números en reales y por eso el double.parse()

--- DANITZHA/primer-trimestre/Henry/promedio.dart
+++ DANITZHA/primer-trimestre/Henry/promedio.dart
@@ -5,5 +5,13 @@ void main() {
 
   print("Ingrese la primera nota: ");
   double nota1 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la segunda nota: ");
+  double nota2 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la tercera nota: ");
+  double nota3 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la cuarta nota: ");
+  double nota4 = double.parse(stdin.readLineSync()!);
+  print("Ingrese la quinta nota: ");
+  double nota5 = double.parse(stdin.readLineSync()!);
 
 }
